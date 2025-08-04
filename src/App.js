5daf2cdecf8e06b512261beb0188eab432c0ce5a
Your Name <victordenny247@gmail.com>
2025-08-04 06:34:39 +0300

    import route

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import React, {useState} from 'react';
 import MovieList from './components/MovieList';
 import Filter from './components/Filter';
 import AddMovie from './components/AddMovie';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [movies, setMovies] = useState([
-    {
-      title: "Breaking Bad",
-      description: "A chemistry teacher turned meth producer.",
-      posterURL: "https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png",
-      rating: 9.5
-    },
-    {
-      title: "Inception",
-      description: "A mind-bending thriller by Christopher Nolan.",
-      posterURL: "https://m.media-amazon.com/images/I/51nbVEuw1HL.jpg",
-      rating: 8.8
-    }
-  ]);
-
+  {
+    id: 1,
+    title: "Ford v Ferrari",
+    description: "American car designer Carroll Shelby and driver Ken Miles battle corporate interference and the laws of physics to build a revolutionary race car for Ford in order to defeat Ferrari at the 24 Hours of Le Mans in 1966.",
+    posterURL: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQgEOmv3LFX6oviPRrBpNkfbkF3hgMSF2rZwNUzYSRBAEQxGprXeMGgkEm9eBRDOl9iXP2Q",
+    rating:8.1,
+    trailerLink: "https://www.imdb.com/video/vi1737539353/?playlistId=tt1950186&ref_=ext_shr_lnk"
+  },
+  {
+    id: 2,
+    title: "Transformers: Rise of the Beasts",
+    description: "Optimus Prime and the Autobots team up with a down on his luck young man, an aspiring historian and with a powerful faction of Transformers known as the Maximals to combat a sinister force from outer space that threatens the Earth and all of mankind.",
+    posterURL: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSEsFNeH_1HnJrtVLrbLdsdamTUTZwn7Kj9YBsSWiUHR4shBKafAY89uQwV_A0Z3Chj_ahPIA",
+    rating:6.0,
+    trailerLink: "https://www.imdb.com/video/vi4232692761/?playlistId=tt5090568&ref_=ext_shr_lnk"
+  }
+]);
   const [titleFilter, setTitleFilter] = useState('');
   const [rateFilter, setRateFilter] = useState('');
 
